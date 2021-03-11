17b65302130f0f9fb511a7e200d4117dc278fb89
sorochanK <sorochank7@gmail.com>
2021-03-11 20:18:04 +0200

    added Game to main method

--- src/main/java/com/snakes/game/GameApplication.java
+++ src/main/java/com/snakes/game/GameApplication.java
@@ -1,5 +1,6 @@
 package com.snakes.game;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GameApplication implements CommandLineRunner {
 
+    @Autowired
+    private Game game;
 
     public static void main(String[] args) {
         SpringApplication.run(GameApplication.class, args);
@@ -15,5 +18,6 @@ public class GameApplication implements CommandLineRunner {
     @Override
     public void run(String... args) {
         System.out.println("Welcome to the game!");
+        game.startGame();
     }
 }
